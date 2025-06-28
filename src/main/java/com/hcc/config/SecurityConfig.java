8bc9f556d75b38bab31a1c4989c2f646962d070c
Collins-Webdev <collinshaya@gmail.com>
2025-06-28 20:29:13 +0100

    first ticket resolved

---
+++ src/main/java/com/hcc/config/SecurityConfig.java
@@ -0,0 +1,37 @@
+package com.hcc.config;
+
+import com.hcc.services.UserDetailServiceImpl;
+import com.hcc.utils.CustomPasswordEncoder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private UserDetailServiceImpl userDetailService;
+
+    @Autowired
+    private CustomPasswordEncoder customPasswordEncoder;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailService).passwordEncoder(customPasswordEncoder.getPasswordEncoder());
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/api/auth/**").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+    }
+}
---
+++ src/main/java/com/hcc/dto/AssignmentResponseDto.java
@@ -0,0 +1,33 @@
+package com.hcc.dto;
+
+import com.hcc.entities.Assignment;
+import com.hcc.enums.AssignmentEnum;
+import com.hcc.enums.AssignmentStatusEnum;
+
+public class AssignmentResponseDto {
+    private Assignment assignment;
+    private AssignmentEnum[] assignmentEnums = AssignmentEnum.values();
+    private AssignmentStatusEnum[] statusEnums = AssignmentStatusEnum.values();
+
+    public AssignmentResponseDto(Assignment assignment) {
+        this.assignment = assignment;
+    }
+
+    // Getters
+    public Assignment getAssignment() {
+        return assignment;
+    }
+
+    public AssignmentEnum[] getAssignmentEnums() {
+        return assignmentEnums;
+    }
+
+    public AssignmentStatusEnum[] getStatusEnums() {
+        return statusEnums;
+    }
+
+    // Setters
+    public void setAssignment(Assignment assignment) {
+        this.assignment = assignment;
+    }
+}
